7d3dbdae2206c59649f8b50b79f18687e6053967
Claire Hanson <clairecatohanson@gmail.com>
2024-05-03 11:40:11 -0500

    styled equipment details page except modal

--- src/components/ShortMaintenanceList.jsx
+++ src/components/ShortMaintenanceList.jsx
@@ -7,36 +7,68 @@ export const ShortMaintenanceList = ({
 }) => {
   return (
     <div>
-      <h3>{title}</h3>
-      {maintenanceList.map((ticket) => (
-        <div key={`ticket-${ticket.id}`}>
-          <h4>
-            {admin ? (
-              <Link to={`/maintenance/${ticket.id}/edit`}>
-                {ticket.maintenance.name}
-              </Link>
-            ) : (
-              ticket.maintenance.name
-            )}
-          </h4>
-          <div>
-            Date Needed: {ticket.date_needed} (Requested by{" "}
-            {ticket.user.first_name} {ticket.user.last_name})
-          </div>
-          <div>
-            Date Scheduled:{" "}
-            {ticket.date_scheduled ? ticket.date_scheduled : "Pending"}
-          </div>
-          <div>
-            Suggested Maintenance Interval:{" "}
-            {ticket.maintenance.days_interval ? (
-              <div>{ticket.maintenance.days_interval} Days</div>
-            ) : (
-              <div>One-time maintenance</div>
-            )}
+      {maintenanceList.length ? (
+        <div>
+          <h3 className="dashboard-heading">{title}</h3>
+          <div className="flex flex-col space-y-2 mb-6">
+            {maintenanceList.map((ticket) => (
+              <div
+                className="p-2 bg-bluegreen-300 rounded"
+                key={`ticket-${ticket.id}`}
+              >
+                <h4 className="font-bold">
+                  {admin ? (
+                    <Link to={`/maintenance/${ticket.id}/edit`}>
+                      {ticket.maintenance.name}
+                    </Link>
+                  ) : (
+                    ticket.maintenance.name
+                  )}
+                </h4>
+                <div className="italic">
+                  (Requested by {ticket.user.first_name} {ticket.user.last_name}
+                  )
+                </div>
+                <div className="mb-2">
+                  <div>Suggested Maintenance Interval: </div>
+                  <div>
+                    {ticket.maintenance.days_interval ? (
+                      <div>{ticket.maintenance.days_interval} Days</div>
+                    ) : (
+                      <div>One-time maintenance</div>
+                    )}
+                  </div>
+                </div>
+                <div className="flex space-x-2">
+                  <div className="min-w-32">Date Needed: </div>
+                  <div className="min-w-24">{ticket.date_needed}</div>
+                </div>
+                <div className="flex space-x-2">
+                  <div className="min-w-32">Date Scheduled: </div>
+                  <div className="min-w-24">
+                    {ticket.date_scheduled ? ticket.date_scheduled : "Pending"}
+                  </div>
+                </div>
+                <div className="flex space-x-2">
+                  <div className="min-w-32">Date Completed: </div>
+                  <div className="min-w-24">
+                    {ticket.date_completed
+                      ? ticket.date_completed.split("T")[0]
+                      : "Pending"}
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-      ))}
+      ) : (
+        <div>
+          <h3 className="dashboard-heading">{title}</h3>
+          <div className="mb-4">
+            There are no {title.toLowerCase()} tickets for this equipment
+          </div>
+        </div>
+      )}
     </div>
   )
 }
