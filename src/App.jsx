6f41e3f80c7a782ae16a945d0ebb569501300cd5
Claire Hanson <clairecatohanson@gmail.com>
2024-05-15 15:16:16 -0500

    added register and select lab forms

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom"
 import { ApplicationViews } from "./views/ApplicationViews"
 import { AppWrapper } from "./context/AppWrapper"
 import { Login } from "./pages/Login"
+import { Register } from "./pages/Register"
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route
         path="*"
         element={
